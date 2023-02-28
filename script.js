815be3f0bf3465aabd4890149d95e93e7dcf5e34
rpgs7 <31049189+rpgs7@users.noreply.github.com>
2023-02-28 17:43:44 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body {
+        background: orange;
+      }
+    </style>
+    <meta http-equiv="refresh" content="0; url='https://ryanandkirsten2024.wixsite.com/goose-nuptials'" />
+  </head>
+  <body>
+    <p>If the redirect has not loaded, please follow <a href="https://ryanandkirsten2024.wixsite.com/goose-nuptials">this link</a>.</p>
+  </body>
+</html>
